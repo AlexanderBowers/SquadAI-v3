9896414015c74f1e1c5372bee257492b4d6c430a
AlexanderBowers <7319abowers@gmail.com>
2024-02-23 22:08:37 -0800

    Beginning definitions for SquadPlayerController.

--- Source/Squadv3/SquadPlayerController.cpp
+++ Source/Squadv3/SquadPlayerController.cpp
@@ -3,3 +3,50 @@
 
 #include "SquadPlayerController.h"
 
+void ASquadPlayerController::BeginPlay()
+{
+}
+
+void ASquadPlayerController::Tick(float DeltaTime)
+{
+}
+
+void ASquadPlayerController::SetupInputComponent()
+{
+}
+
+FCommandPoint ASquadPlayerController::CreateCommandPoint(FHitResult Hitresult)
+{
+	return FCommandPoint();
+}
+
+FCommandPoint ASquadPlayerController::AssignLocation(FCommandPoint CommandPoint, FHitResult HitResult)
+{
+	return FCommandPoint();
+}
+
+FCommandPoint ASquadPlayerController::AssignType(FCommandPoint CommandPoint, FHitResult Hitresult)
+{
+	return FCommandPoint();
+}
+
+void ASquadPlayerController::DeployInvestigate(FCommandPoint CommandPoint)
+{
+}
+
+void ASquadPlayerController::SetNewAITarget(AActor* NewTarget)
+{
+}
+
+void ASquadPlayerController::MoveUpCommand()
+{
+}
+
+void ASquadPlayerController::FormUpCommand()
+{
+}
+
+TArray<AActor*> ASquadPlayerController::GetRooms(AActor* Building)
+{
+	return TArray<AActor*>();
+}
