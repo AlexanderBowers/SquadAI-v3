f67f81b23bc3505f8b4b135209e10649bf7b5e12
AlexanderBowers <7319abowers@gmail.com>
2024-02-23 22:33:18 -0800

    Started declaring functions for SquadAIController

---
+++ Source/Squadv3/SquadAIController.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SquadAIController.h"
+#include "SquadPlayerController.h"
+
+void ASquadAIController::BeginPlay()
+{
+}
+
+void ASquadAIController::Tick(float DeltaTime)
+{
+}
+
+void ASquadAIController::StartFollow()
+{
+}
+
+void ASquadAIController::StopFollow()
+{
+}
+
+void ASquadAIController::MoveToCommand(FCommandPoint CommandPoint)
+{
+}
+
+void ASquadAIController::HandleCommand()
+{
+}
+
+void ASquadAIController::StopCommandAndFollow()
+{
+}
+
+void ASquadAIController::StopCommandDontFollow()
+{
+}
+
+void ASquadAIController::ClearRoom(FVector RoomLocation)
+{
+}
