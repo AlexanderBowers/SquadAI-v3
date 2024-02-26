fcdc430e9b92b21d049cc50fe910ffa5418e80b3
AlexanderBowers <7319abowers@gmail.com>
2024-02-25 19:45:22 -0800

    Commandos are now firing on begin play

---
+++ enc_temp_folder/4adc835731c29d1e0634982693f87d4/SquadAIController.h
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "SquadPlayerController.h"
+#include "TimerManager.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "SquadAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SQUADV3_API ASquadAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+
+	//Command references
+	FCommandPoint LastCommand;
+	TArray<FCommandPoint> LocalCommandList; //In case I need to store multiple commands
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	AActor* AssignedPosition;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	AActor* AssignedFollow;
+
+	//Commands
+	UFUNCTION(BlueprintCallable)
+	void StartFollow();
+	UFUNCTION(BlueprintCallable)
+	void StopFollow();
+	UFUNCTION(BlueprintCallable)
+	void MoveToCommand(FCommandPoint CommandPoint);
+	UFUNCTION()
+	void HandleCommand(FCommandPoint CommandPoint);
+	UFUNCTION(BlueprintCallable)
+	void StopCommandAndFollow();
+	UFUNCTION(BlueprintCallable)
+	void StopCommandDontFollow();
+	UFUNCTION()
+	void ClearRoom(FVector RoomLocation);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UBlackboardComponent* TheBlackboard;
+	UPROPERTY()
+	ASquadPlayerController* PlayerController;
+	UPROPERTY()
+	FTimerHandle TimerHandle;
+
+
+	
+};
